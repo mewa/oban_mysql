f06440ca6341f6dd7a0f43790b86eb34e786718a
mewa <marcin.k.chmiel@gmail.com>
2024-05-05 01:46:28 +0200

    (WIP) unique jobs

--- lib/oban/engines/mysql.ex
+++ lib/oban/engines/mysql.ex
@@ -31,6 +31,18 @@ defmodule Oban.Engines.MySQL do
     end
   end
 
+  defmacrop json_contains(column, object) do
+    quote do
+      fragment(
+        """
+        json_contains(?, ?, '$')
+        """,
+        unquote(column),
+        unquote(object)
+      )
+    end
+  end
+
   @impl Engine
   defdelegate init(conf, opts), to: Basic
 
@@ -47,7 +59,9 @@ defmodule Oban.Engines.MySQL do
   defdelegate shutdown(conf, meta), to: Basic
 
   @impl Engine
-  defdelegate insert_job(conf, changeset, opts), to: Basic
+  def insert_job(conf, changeset, opts) do
+    insert_unique(conf, changeset, opts)
+  end
 
   @impl Engine
   defdelegate insert_all_jobs(conf, changesets, opts), to: Basic
@@ -202,6 +216,151 @@ defmodule Oban.Engines.MySQL do
 
   @impl Engine
   defdelegate retry_all_jobs(conf, queryable), to: Basic
+
+  # Insertion
+
+  defp insert_unique(conf, changeset, opts) do
+    opts = Keyword.put(opts, :on_conflict, :nothing)
+
+    lock_info = unique_query(changeset)
+    try do
+      {:ok, result} = Repo.transaction(conf, fn ->
+        with {:ok, query, lock_key} <- lock_info,
+             :ok <- acquire_lock(conf, lock_key),
+             {:ok, job} <- fetch_job(conf, query, opts),
+             {:ok, job} <- resolve_conflict(conf, job, changeset, opts) do
+
+          {:ok, %Job{job | conflict?: true}}
+        else
+          {:error, :locked} ->
+            Changeset.apply_action(changeset, :insert)
+
+          nil ->
+            Repo.insert(conf, changeset, opts)
+
+          error ->
+            error
+        end
+
+      end)
+
+      result
+    after
+      with {:ok, _, lock_key} <- lock_info do
+        release_lock(conf, lock_key)
+      end
+    end
+    
+  end
+
+  defp resolve_conflict(conf, job, changeset, opts) do
+    case Changeset.fetch_change(changeset, :replace) do
+      {:ok, replace} ->
+        keys = Keyword.get(replace, String.to_existing_atom(job.state), [])
+
+        Repo.update(
+          conf,
+          Changeset.change(job, Map.take(changeset.changes, keys)),
+          opts
+        )
+
+      :error ->
+        {:ok, job}
+    end
+  rescue
+    error in [Ecto.StaleEntryError] ->
+      {:error, error}
+  end
+
+  defp unique_query(%{changes: %{unique: %{} = unique}} = changeset) do
+    %{fields: fields, keys: keys, period: period, states: states, timestamp: timestamp} = unique
+
+    keys = Enum.map(keys, &to_string/1)
+    states = Enum.map(states, &to_string/1)
+    dynamic = Enum.reduce(fields, true, &unique_field({changeset, &1, keys}, &2))
+    lock_key = :erlang.phash2([keys, states, dynamic])
+
+    query =
+      Job
+      |> where([j], j.state in ^states)
+      |> since_period(period, timestamp)
+      |> where(^dynamic)
+      |> limit(1)
+
+    {:ok, query, lock_key}
+  end
+
+  defp unique_query(_changeset), do: nil
+
+  defp unique_field({changeset, field, keys}, acc) when field in [:args, :meta] do
+    value = unique_map_values(changeset, field, keys)
+
+    cond do
+      value == %{} ->
+        dynamic([j], json_contains(^value, field(j, ^field)) and ^acc)
+
+      keys == [] ->
+        dynamic(
+          [j],
+          json_contains(field(j, ^field), ^value) and
+            json_contains(^value, field(j, ^field)) and ^acc
+        )
+
+      true ->
+        dynamic([j], json_contains(field(j, ^field), ^value) and ^acc)
+    end
+  end
+
+  defp unique_field({changeset, field, _}, acc) do
+    value = Changeset.get_field(changeset, field)
+
+    dynamic([j], field(j, ^field) == ^value and ^acc)
+  end
+
+  defp unique_map_values(changeset, field, keys) do
+    case keys do
+      [] ->
+        Changeset.get_field(changeset, field)
+
+      [_ | _] ->
+        changeset
+        |> Changeset.get_field(field)
+        |> Map.new(fn {key, val} -> {to_string(key), val} end)
+        |> Map.take(keys)
+    end
+  end
+
+  defp since_period(query, :infinity, _timestamp), do: query
+
+  defp since_period(query, period, timestamp) do
+    where(query, [j], field(j, ^timestamp) >= ^seconds_from_now(-period))
+  end
+
+  defp acquire_lock(%{testing: :disabled} = conf, base_key) do
+    pref_key = :erlang.phash2(conf.prefix)
+    lock_key = "oban_#{pref_key + base_key}"
+
+    case Repo.query(conf, "SELECT GET_LOCK(?, ?)", [lock_key, 0]) do
+      {:ok, %{rows: [[1]]}} -> :ok
+      _ -> {:error, :locked}
+    end
+  end
+
+  defp acquire_lock(_conf, _key), do: :ok
+
+  defp release_lock(conf, base_key) do
+    pref_key = :erlang.phash2(conf.prefix)
+    lock_key = "oban_#{pref_key + base_key}"
+
+    Repo.query(conf, "SELECT RELEASE_LOCK(?)", [lock_key])
+  end
+
+  defp fetch_job(conf, query, opts) do
+    case Repo.one(conf, query, Keyword.put(opts, :prepare, :unnamed)) do
+      nil -> nil
+      job -> {:ok, job}
+    end
+  end
 
   defp seconds_from_now(seconds), do: DateTime.add(utc_now(), seconds, :second)
 
